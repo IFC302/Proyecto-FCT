6848af5ef8dbcda5db73780b4df367c1f900ec97
dam2d26 <57983385+dam2d26@users.noreply.github.com>
2020-05-08 20:28:03 +0200

    Movimiento listo

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static List<Enemy> Enemies = new List<Enemy>();
+
+    enum State
     {
-        
+        Idle,
+        Running,
+        Attacking,
+        Dead
     }
 
-    // Update is called once per frame
-    void Update()
+    State _state = State.Idle;
+
+    Animator _ac;
+
+    int lives = 5;
+
+    // Para que el jugador no lo considere como un objetivo válido
+    public bool IsAlive
     {
-        
+        // Solo queremos que esté vivo si las vidas son > 0
+        get { return lives > 0; }
+    }
+
+    void Start()
+    {
+        _ac = GetComponent<Animator>();
+    }
+
+    // Cuándo le pegan
+    public void Hit(int damage)
+    {
+        if (lives <= 0)
+        {
+            //SetState(State.Dead);
+        }
+    }
+
+    // Cuándo los zombis se activan se registran
+    void OnEnable()
+    {
+        Enemies.Add(this);
+    }
+
+    // Cuándo los zombis mueren se retiran
+    void OnDisable()
+    {
+        Enemies.Remove(this);
+    }
+
+    void SetState(State state)
+    {
+        if (state != _state)
+        {
+            _state = state;
+            switch (_state)
+            {
+                case State.Running:
+                    _ac.CrossFade("Run", 0.1f);
+                    break;
+                case State.Idle:
+                    _ac.CrossFade("Idle", 0.1f);
+                    break;
+                case State.Attacking:
+                    _ac.CrossFade("Attack", 0.1f);
+                    break;
+                case State.Dead: // Cuándo muere
+                    // Animación de morir
+                    _ac.CrossFade("Die", 0.1f);
+                    // Si hacemos un destroy de this va a destruir el componente Enemy, no el objeto
+                    Destroy(gameObject, 2f); // Destruimos el objeto en 2 sec
+                    break;
+            }
+        }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -26,15 +26,147 @@ public class Player : MonoBehaviour
     [SerializeField]
     float _range = 15f;
 
-    // Start is called before the first frame update
+    State _state;
+    Enemy _target;
+    AudioSource _audio;
+
     void Start()
     {
-        
+        _ac = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Si el joystick se está moviento no se actualizan los estados
+        // Si dejamos de pulsar el joystick va a pasar de corriendo a idle
+        // De idle a buscar al zombi
+        // Y de encontrar al zombi a disparar
+
+        // Si el joystick no se está moviendo
+        if (_input.Direction.sqrMagnitude > 0f)
+        {
+            // Se mueve
+            var direction = transform.forward;
+            transform.position += direction * Time.deltaTime * _speed;
+            transform.LookAt(transform.position + (new Vector3(_input.Direction.x, 0f, _input.Direction.y)));
+            SetState(State.Running);
+        }
+        else
+        {
+            UpdateState();
+        }
+    }
+
+    // Evento al que nos llama la animación cada vez que llega a ese frame (justo al principio)
+    public void Shoot()
+    {
+        _audio.Play();
+        _particles.Play();
+        if (_target != null) // Si tiene al zombi a la vista
+        {
+            // Le hace 1 de daño
+            _target.Hit(1);
+            // Le apuntamos
+            _line.enabled = true;
+            var target = _target.transform.position;
+            target.y = 1f;
+            _line.SetPosition(0, _particles.transform.position);
+            _line.SetPosition(1, target);
+            StopAllCoroutines();
+            StartCoroutine(Recoil());
+        }
+    }
+
+    // Volver a esconder la línea
+    IEnumerator Recoil()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _line.enabled = false;
+    }
+
+    // Comprueba si el enemigo está en rango
+    bool IsInRange(Enemy enemy)
+    {
+        var dist = (enemy.transform.position - transform.position).sqrMagnitude;
+        return dist < _range * _range;
+    }
+
+    // Busca al enemigo mas cercano
+    Enemy FindClosesEnemyInRange()
+    {
+        float closest = int.MaxValue;
+        Enemy candidate = null;
+        float sqRange = _range * _range;
+        foreach (var enemy in Enemy.Enemies)
+        {
+            var dist = (enemy.transform.position - transform.position).sqrMagnitude;
+            if (enemy.IsAlive && dist < closest && dist < sqRange)
+            {
+                closest = dist;
+                candidate = enemy;
+            }
+        }
+        return candidate;
+    }
+
+    // Dependiendo de cada estado se va a actualizar
+    void UpdateState()
+    {
+        switch (_state)
+        {
+            case State.Running:
+                SetState(State.Idle); // Si no estamos moviendo el Joystick y está corriendo pasa a Idle
+                break;
+            case State.Idle:
+                // Busca al enemigo más cercano
+                Enemy candidate = FindClosesEnemyInRange();
+                // Si lo encuentra
+                if (candidate != null)
+                {
+                    // Lo ataca
+                    _target = candidate; // Guarda a quien estoy disparando
+                    transform.LookAt(_target.transform); // Lo mira
+                    SetState(State.Attacking); // Lo ataca
+                }
+                break;
+            case State.Attacking:
+                // Si se ha cargado al objetivo
+                // O no tiene objetivo
+                // O el objetivo se ha pirado de su rango
+                if (_target == null || !_target.IsAlive || !IsInRange(_target))
+                {
+                    // Pasamos a buscar objetivo de nuevo
+                    SetState(State.Idle);
+                }
+                break;
+            case State.Dead:
+                break;
+        }
+    }
+
+    // Cambia el estado del poli
+    void SetState(State state)
+    {
+        // Si el estado es diferente al actual
+        if (state != _state)
+        {
+            _state = state;
+            switch (_state)
+            {
+                case State.Running:
+                    _ac.CrossFade("Run", 0.1f);
+                    break;
+                case State.Idle:
+                    _ac.CrossFade("Idle", 0.1f);
+                    break;
+                case State.Attacking:
+                    _ac.CrossFade("Attack", 0.1f);
+                    break;
+                case State.Dead:
+                    _ac.CrossFade("Die", 0.1f);
+                    break;
+            }
+        }
     }
 }
